386e49a482c233405e0813a45aa74f75bb1e9479
Yerson Cadillo <cadilloyerson@gmail.com>
2026-06-10 13:59:31 -0500

    Agrega cambios en el pom, config de pruebas y DataLoader

--- voltroom-backend/src/main/java/com/voltroom/voltroombackend/config/DataLoader.java
+++ voltroom-backend/src/main/java/com/voltroom/voltroombackend/config/DataLoader.java
@@ -6,9 +6,11 @@ import com.voltroom.voltroombackend.repository.RolRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("!test")
 public class DataLoader implements CommandLineRunner {
 
     private final RolRepository rolRepository;
